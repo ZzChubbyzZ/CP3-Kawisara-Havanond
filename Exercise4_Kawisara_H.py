bcf6e8fac5318fcdd2fee93d4b28b895a7d6d912
ZzChubbyzZ <82933776+ZzChubbyzZ@users.noreply.github.com>
2021-04-26 20:41:32 +0700

    Add files via upload

---
+++ Exercise4_Kawisara_H.py
@@ -0,0 +1,9 @@
+FE = 70
+GB = 80.5
+ICS = 60
+CP = 77.3
+print("--------Your Score--------")
+print("Foundation English:", FE)
+print( "General Business:", GB)
+print("Introduction to Computer System:", ICS)
+print("Computer Programming:", CP)
